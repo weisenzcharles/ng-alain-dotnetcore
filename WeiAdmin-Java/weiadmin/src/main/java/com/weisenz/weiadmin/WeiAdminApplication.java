032b951c709dc00fcbe6e2b019c91e839176eb16
Charles Zhang <master@weilog.net>
2021-01-14 16:52:59 +0800

    调整项目结构

---
+++ WeiAdmin-Java/weiadmin/src/main/java/com/weisenz/weiadmin/WeiAdminApplication.java
@@ -0,0 +1,13 @@
+package com.weisenz.weiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiAdminApplication.class, args);
+    }
+
+}
